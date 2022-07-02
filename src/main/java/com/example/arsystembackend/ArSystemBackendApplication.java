a0197d3bd77376d57ae4f95a02fcbe00879ce078
unclechuen <33083306+unclechuen@users.noreply.github.com>
2022-07-02 17:17:59 +0800

    Initial commit

---
+++ src/main/java/com/example/arsystembackend/ArSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.arsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arsystembackend/ArSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
